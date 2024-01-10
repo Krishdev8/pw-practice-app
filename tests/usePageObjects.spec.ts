9390815cdc17798fc8b23b0ddebcdd8dd91918df
Krishna Ghoman <ghomankrishna9@gmail.com>
2024-01-10 13:01:50 +0530

    Add date picker functionality to form layouts test

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
 import {FormLayoutPage} from '../page-objects/formLayoutsPage'
+import { datePickerPage } from '../page-objects/datePickerPage'
 
 test.beforeEach(async({page})=>{
     await page.goto('http://localhost:4200/')
@@ -19,7 +20,14 @@ test('parametrized methods', async({page})=>{
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutPage(page)
 
+    const onDatePickerPage = new datePickerPage(page)
+
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('krish@krishna.com','Balram1', 'Option 2')
     await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('Krishna', 'krish@krishna.com', false)
+
+    await navigateTo.datePickerPage()
+    await onDatePickerPage.selectCommeonDatePickerDateFromToday(10)
+
+    await onDatePickerPage.selectDatePickerWithRangeFromToday(10, 20)
 })
